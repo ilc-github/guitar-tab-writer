57c4f70ab4035293af87118b62faebf17ef78c4f
ilc-github <irving.chou@gmail.com>
2016-12-01 12:52:49 -0800

    Dropping scrape for now

--- src/scrape.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import urllib
-r = urllib.urlopen('https://www.gametabs.net/elder-scrolls-v-skyrim/sons-skyrim').read()
-soup = BeautifulSoup(r)
-print type(soup)
-
-''' Access denied.  Cloudflare?
-'''
